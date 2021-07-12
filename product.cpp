b9955cf8b6e4e20fa72dc72c038dae135ee5d27f
Roshan Gupta <gpta.duke.rohan.1219@gmail.com>
2021-07-12 10:19:05 +0530

    first commit

---
+++ product.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    cout << "Enter first number : ";
+    cin >> a;
+    cout << "Enter first number : ";
+    cin >> b;
+    cout << "Product of " << a << " and " << b << " is " << a * b << endl;
+
+    return 0;
+}
---
+++ size.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Size of Char is " << sizeof(char) << endl;
+    cout << "Size of Int is " << sizeof(int) << endl;
+    cout << "Size of Float is " << sizeof(float) << endl;
+    cout << "Size of Double is " << sizeof(double) << endl;
+
+    return 0;
+}
